6ee50a3380245fc0fbe0d1ff9ae589465ce7cfa6
Mai Hoa <hoa.btm1309@gmail.com>
2023-08-29 16:16:24 +0700

    sua lai repository

--- Repository/ContestRepository.cs
+++ Repository/ContestRepository.cs
@@ -1,4 +1,5 @@
-﻿using Project3.Data;
+﻿using Microsoft.AspNetCore.Identity;
+using Project3.Data;
 using Project3.Models;
 
 namespace Project3.Repository
@@ -9,8 +10,6 @@ namespace Project3.Repository
     }
     public class ContestRepository : BaseRepository<Contest>, IContestRepository
     {
-        public ContestRepository(ApplicationDbContext context) : base(context)
-        {
-        }
+        public ContestRepository(ApplicationDbContext dbContext, UserManager<CustomUser> userManager, IHttpContextAccessor httpContext) : base(dbContext, userManager, httpContext) { }
     }
 }
--- Repository/RegisterRepository.cs
+++ Repository/RegisterRepository.cs
@@ -1,4 +1,5 @@
-﻿using Project3.Data;
+﻿using Microsoft.AspNetCore.Identity;
+using Project3.Data;
 using Project3.Models;
 
 namespace Project3.Repository
@@ -9,8 +10,6 @@ namespace Project3.Repository
     }
     public class RegisterRepository : BaseRepository<Register>, IRegisterRepository
     {
-        public RegisterRepository(ApplicationDbContext context) : base(context)
-        {
-        }
+        public RegisterRepository(ApplicationDbContext dbContext, UserManager<CustomUser> userManager, IHttpContextAccessor httpContext) : base(dbContext, userManager, httpContext) { }
     }
 }
