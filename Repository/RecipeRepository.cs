9821ba43424956a1b025c39a253b879237c32f38
tho <tho@gmail.com>
2023-09-06 20:36:46 +0700

    co len ban oiii

--- Repository/RecipeRepository.cs
+++ Repository/RecipeRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Project3.Data;
 using Project3.Models;
 
@@ -11,6 +12,13 @@ namespace Project3.Repository
     public class RecipeRepository : BaseRepository<Recipe>, IRecipeRepository
     {
         public RecipeRepository(ApplicationDbContext dbContext, UserManager<CustomUser> userManager, IHttpContextAccessor httpContext) : base(dbContext, userManager, httpContext) { }
-
+        public List<Recipe> GetLatestCreatedRecipes(int count)
+        {
+            // Sử dụng LINQ để truy vấn dữ liệu và lấy danh sách bản ghi được tạo gần nhất
+            List<Recipe> latestRecipes = _context.Recipes.OrderByDescending(r => r.CreatedTime)
+                                                       .Take(count)
+                                                       .ToList();
+            return latestRecipes;
+        }
     }
 }
