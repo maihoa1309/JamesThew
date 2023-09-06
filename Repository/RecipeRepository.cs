568b5cedd287efbd494e31656aef64e7be89becd
Mai Hoa <hoa.btm1309@gmail.com>
2023-09-06 20:35:47 +0700

    add recipe

--- Repository/RecipeRepository.cs
+++ Repository/RecipeRepository.cs
@@ -1,16 +1,83 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Project3.Data;
 using Project3.Models;
+using Project3.DTO;
+using Microsoft.Extensions.Primitives;
 
 namespace Project3.Repository
 {
     public interface IRecipeRepository : IBaseRepository<Recipe>
     {
-
+        Task<bool> CreateRecipe(FormAddRecipe request);
     }
     public class RecipeRepository : BaseRepository<Recipe>, IRecipeRepository
     {
         public RecipeRepository(ApplicationDbContext dbContext, UserManager<CustomUser> userManager, IHttpContextAccessor httpContext) : base(dbContext, userManager, httpContext) { }
 
-    }
+		public async Task<bool>  CreateRecipe(FormAddRecipe request)
+		{
+			var currentUser = _userManager.GetUserAsync(_contextAccessor.HttpContext.User).GetAwaiter().GetResult();
+
+			var recipe = new Recipe();
+			recipe.Title = request.Title;
+			recipe.Description = request.Description;
+			recipe.CookingTime = request.CookingTime;
+			recipe.Servings = request.Servings;
+			recipe.Instruction = request.Instruction;
+			recipe.UserId = currentUser.Id;
+			recipe.Cuisines = request.Cuisines;
+			recipe.IsFree = Convert.ToBoolean(request.IsFree);
+			_dbSet.Add(recipe);
+			_context.SaveChanges();
+			var recipeId = recipe.Id;
+			//upload anh 
+			recipe.Img = "";
+			List<IFormFile> imgs = new List<IFormFile>
+			{
+				request.Img1,
+				request.Img2,
+				request.Img3
+			};
+			foreach(var img in imgs) 
+			{
+				if (img != null)
+				{
+					//dat lai ten anh theo guid + phan extension
+					string uploadImgName = Guid.NewGuid() + Path.GetExtension(img.FileName);
+					//upload file
+					string savePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/UploadImg", uploadImgName);
+					//tao file moi vao duong dan upload copy img vao SreamUploadFile?
+					using (var StreamUploadFile = new FileStream(savePath, FileMode.CreateNew))
+					{
+						img.CopyTo(StreamUploadFile);
+					}
+					recipe.Img += uploadImgName;
+				}
+			}
+
+			
+			
+			string[] ingres = new string[]
+			{
+				request.Ingredient,
+				request.Ingredient1,
+				request.Ingredient2,
+				request.Ingredient3,
+				request.Ingredient4,
+				request.Ingredient5
+			};
+			string[] quantities = new string[]
+			{
+				request.Quantity,
+				request.Quantity1,
+				request.Quantity2,
+				request.Quantity3,
+				request.Quantity4,
+				request.Quantity5
+			};
+		
+
+			return true;
+		}
+	}
 }
