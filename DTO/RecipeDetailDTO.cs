7383c052b117bc0b1ddb3c1b5f76026e5f5144d1
Mai Hoa <hoa.btm1309@gmail.com>
2023-09-07 11:21:17 +0700

    add recipe

---
+++ DTO/RecipeDetailDTO.cs
@@ -0,0 +1,20 @@
+﻿using Project3.Models;
+
+namespace Project3.DTO
+{
+    public class RecipeDetailDTO
+    {
+        public Recipe? Recipe { get; set; }
+        public List<Ingredient>? Ingredients { get; set; }
+        public string? UserName { get; set; }
+    }
+    public class  Ingredient
+    {
+        public int? RecipeId { get; set; }  
+        public int? IngredientId { get; set; }
+        public string? Name { get; set; }
+        public string? Quantity { get; set; }
+
+
+    }
+}
