be9ed6896dbcc9db46bc811dedbf16aa2b1d40a2
Mai Hoa <hoa.btm1309@gmail.com>
2023-10-01 19:34:22 +0700

    new v

---
+++ DTO/RegisterDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Project3.DTO
+{
+    public class RegisterDTO
+    {
+        public string? UserId { get; set; }
+        public string? TypeMembership { get; set; }
+        public DateTime? FromDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public string? Status { get; set; }
+        public string? Email { get; set; }
+        public string? UserName { get; set; }
+
+    }
+}
